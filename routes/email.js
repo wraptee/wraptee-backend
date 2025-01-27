04367c24d3faa16702456bd84d245758ce2622d0
Rudra <prudra882@gmail.com>
2025-01-27 16:29:44 +0530

    contact us added

--- routes/email.js
+++ routes/email.js
@@ -37,7 +37,7 @@ router.post("/send-email", async (req, res) => {
   }
 });
 
-// POST route to send order details
+// POST route to send orders
 router.post("/send-order", async (req, res) => {
   const { name, email, phoneNumber, cart } = req.body;
 
@@ -82,6 +82,43 @@ router.post("/send-order", async (req, res) => {
     // Send email
     await transporter.sendMail(mailOptions);
     res.status(200).json({ message: "Order email sent successfully" });
+  } catch (error) {
+    console.error("Error sending the email:", error);
+    res.status(500).json({ message: "Failed to send order email", error: error.message });
+  }
+});
+
+// POST route to send contact details and queries
+router.post("/contact-us", async (req, res) => {
+  const { name, email, phoneNumber, message } = req.body;
+
+  try {
+    // Create a transporter
+    const transporter = nodemailer.createTransport({
+      service: "Gmail", // You can use other services like Outlook, Yahoo, etc.
+      auth: {
+        user: process.env.EMAIL, // Your email address
+        pass: process.env.PASSWORD, // Your email password or app-specific password
+      },
+    });
+
+    // Email options
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: process.env.TO_EMAIL, // Destination email address
+      subject: "New Query Recieved",
+      html: `
+        <h3>Order Details</h3>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${phoneNumber}</p>
+        <p><strong>Queries:</strong> ${message}</p>
+      `,
+    };
+
+    // Send email
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "WrapTee will respond you soon" });
   } catch (error) {
     console.error("Error sending the email:", error);
     res.status(500).json({ message: "Failed to send order email", error: error.message });
